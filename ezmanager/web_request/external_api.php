16fa048c25b7fe7bd826494988f34b14180e119a
Mulham Aryan <moulhamo@gmail.com>
2019-08-30 15:31:35 +0200

    Change API Authentication method

--- ezmanager/web_request/external_api.php
+++ ezmanager/web_request/external_api.php
@@ -1,51 +1,45 @@
 <?php
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json");
-    require_once __DIR__."/../config.inc";
-    require_once __DIR__."/../lib_ezmam.php";
-    //require_once __DIR__."/../../commons/lib_sql_management.php";
+    require_once __DIR__ . "/../config.inc";
+    require_once "$basedir/commons/common.inc";
+    require_once "$basedir/ezmanager/lib_ezmam.php";
+    require_once "$basedir/commons/api.credentials.inc";
+    if ($enable_api_access == 1) {
+        $input = array_merge($_POST, $_GET);
+        $ApiAction = isset($input['ApiAction']) ? $input['ApiAction'] : NULL;
+        $ApiClient = isset($input['ApiClient']) ? $input['ApiClient'] : NULL;
+        $ApiUserid = isset($input['ApiUserid']) ? $input['ApiUserid'] : NULL;
+        $ApiSecret = isset($input['ApiSecret']) ? $input['ApiSecret'] : NULL;
+        $ApiNbrElm = isset($input['ApiNbrElm']) ? (int)$input['ApiNbrElm'] : 1;
 
+        $ApiLogin = 0;
 
-    $input = array_merge($_POST, $_GET);
-    $ApiAction = isset($input['ApiAction']) ? $input['ApiAction'] : NULL;
-    $ApiClient = isset($input['ApiClient']) ? $input['ApiClient'] : NULL;
-    $ApiUserid = isset($input['ApiUserid']) ? $input['ApiUserid'] : NULL;
-    $ApiSecret = isset($input['ApiSecret']) ? $input['ApiSecret'] : NULL;
-    $ApiNbrElm = isset($input['ApiNbrElm']) ? (int)$input['ApiNbrElm'] : 1;
-
-    $ApiJsonFile = file_get_contents("$basedir/commons/api.credentials.json");
-    $ApiJson = json_decode($ApiJsonFile, true);
-    $ApiLogin = 0;
-
-    foreach ($ApiJson as $ApiItem) {
-        if($ApiItem["client"] == $ApiClient){
-            if($ApiItem["credentials"]["username"] == $ApiUserid && $ApiItem["credentials"]["secretkey"] == $ApiSecret){
+        if (isset($api[$ApiClient])) {
+            $apiInfo = $api[$ApiClient];
+            if ($apiInfo["ApiUser"] == $ApiUserid && $apiInfo["ApiSecret"] == $ApiSecret && $apiInfo["ApiStatus"] == 1) {
                 $ApiLogin = 1;
-            }
-            else{
+            } else {
                 $ApiLogin = 0;
                 $ApiMsg = "credentials_error";
             }
-            break;
-        }
-        else{
+        } else {
             $ApiLogin = 0;
             $ApiMsg = "client_not_found";
         }
-    }
 
-    if($ApiLogin == 0)
-        echo json_encode($ApiMsg);
+        if ($ApiLogin == 0)
+            echo json_encode($ApiMsg);
 
-    else{
-        switch ($ApiAction) {
-            case 'courses_list':
-                require_once('api/courses_list.php');
-                break;
-            default:
-                echo json_encode('action_not_found');
-                break;
+        else {
+            switch ($ApiAction) {
+                case 'courses_list':
+                    require_once('api/courses_list.php');
+                    break;
+                default:
+                    echo json_encode('action_not_found');
+                    break;
+            }
         }
-    }
-
+    } else echo json_encode('api_access_is_not_enabled')
 ?>
